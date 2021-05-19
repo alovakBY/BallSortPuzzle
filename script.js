8497b86e978129698dde88948ade854c649a6eb3
Nosovitski <nosovitski.gleb@gmail.com>
2021-05-19 22:20:35 +0300

    commit2

--- script.js
+++ script.js
@@ -7,19 +7,17 @@ const opacity08 = document.querySelector(".opacity08") // Контейнер cla
 const opacity = document.querySelector(".opacity") // Контейнер class = "opacity0.8"
 const container = document.querySelector(".container") //Контейнер class = "container"
 const music = new Audio// audio тег
-const soundFinishBottle = new Audio(`/sound/finishBottle.mp3`)
-const soundFinishLvl = new Audio(`/sound/finishLvl.mp3`)
+const soundFinishBottle = new Audio(`sound/finishBottle.mp3`)
+const soundFinishLvl = new Audio(`sound/finishLvl.mp3`)
 const musicSetting = document.querySelector(".settings--music") // контейнер вкл/выкл музыки
 const soundSetting = document.querySelector(".settings--sound") // контейнер вкл/выкл звуков
 const volumeSettings = document.querySelector(".settings--volume")  // контейнер увел/уменьш громкости
 const volumeInput = document.querySelector("input[type = 'range']") // input громкости
 const volumeLine = document.querySelector(".settings--volume--checkbox") // Линия по которой ходит ползунок громкости
 const volumePose = document.querySelector(".settings--volume--checkbox--pose") // Ползунок громкости
-const musicTrackList = ["/sound/soundTrack/bensound-memories.mp3", "/sound/soundTrack/bensound-ukulele.mp3","/sound/soundTrack/bensound-cute.mp3"] // трек-лист
+const musicTrackList = ["sound/soundTrack/bensound-memories.mp3", "sound/soundTrack/bensound-ukulele.mp3","sound/soundTrack/bensound-cute.mp3"] // трек-лист
 buttonStartGame.textContent = "Играть"
 
-soundFinishBottle.load()
-soundFinishLvl.load()
 
 // Функция открыть настройки
 function openSettings () {
@@ -44,7 +42,13 @@ function nextTrack(ind) {
 // Функция срабатывания звуков
 
 function sounds(sound) {
-	if (soundSetting.querySelector("input").checked) sound.play()
+	if (soundSetting.querySelector("input").checked) {
+		sound.load()
+		sound.play()
+		sound.addEventListener("canplaythrough", () => {
+			sound.play()
+		})
+	}
 }
 
 
