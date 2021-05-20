16ba0bdfdf7d38b0b2351a61874ff302e6f229b6
Nosovitski <nosovitski.gleb@gmail.com>
2021-05-20 23:20:41 +0300

    commit

--- script.js
+++ script.js
@@ -48,10 +48,10 @@ function nextTrack(ind) {
 
 function sounds(sound) {
 	if (soundSetting.querySelector("input").checked) {
-		sound.load()
-		sound.addEventListener("canplaythrough", () => {
+		sound.play()
+		/* sound.addEventListener("canplaythrough", () => {
 			sound.play()
-		})
+		}) */
 	}
 }
 
